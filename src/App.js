0c38a5762731f873f0f3558d74efd84ce2cce7c2
Alex Weisberger <alex.weisberger@viewthespace.com>
2019-10-26 19:28:54 -0400

    Factor out DealForm into own component

--- src/App.js
+++ src/App.js
@@ -61,45 +61,27 @@ function DealList(props) {
   />
 }
 
-function StateButton(props) {
-  let [_, updateState] = useContext(StateContext);
-
-  function addDeal() {
-    updateState(stateDraft => { stateDraft.dealManagement.deals.push({ size: 15, tenant: 'Context Tenant', assets: [{ name: 'Context Asset' }]}) });
-  }
-
-  return <Button onClick={addDeal} />
-}
-
-function App() {
-  let [state, setState] = useState({ 
-    dealManagement, 
-    isDealModalVisible: false, 
-    isAssetSearchLoading: false
-  });
-
-  function updateState(command) {
-    setState(produce(state, command));
-  } 
-
-  let appState = [
-    state,
-    updateState
-  ];
-
-  useEffect(() => {
-    updateState(draftState => draftState.dealManagement.viewDeals());
-  }, []);  
+function DealForm(props) {
+  let [state, updateState] = useContext(StateContext);
 
   function handleSize(event) {
     let size = event.target.value;
     updateState(draftState => { draftState.dealManagement.dealForm.size = size });
   }
 
-  function showNewDealForm() {
+  function handleAsset(asset) {
+    updateState(draftState => { draftState.dealManagement.dealForm.assets.push({ name: asset }) });
+  }
+
+  function handleTenant(event) {
+    let tenant = event.target.value;
+    updateState(draftState => { draftState.dealManagement.dealForm.tenant = tenant });
+  }
+
+  function saveDeal() {
     updateState(draftState => { 
-      draftState.dealManagement.makeNewDeal() ;
-      draftState.isDealModalVisible = true;
+      draftState.dealManagement.save();
+      draftState.isDealModalVisible = draftState.dealManagement.errors.length !== 0;
     });
   }
 
@@ -110,22 +92,6 @@ function App() {
     });    
   }
 
-  function saveDeal() {
-    updateState(draftState => { 
-      draftState.dealManagement.save();
-      draftState.isDealModalVisible = draftState.dealManagement.errors.length !== 0;
-    });
-  }
-
-  function handleAsset(asset) {
-    updateState(draftState => { draftState.dealManagement.dealForm.assets.push({ name: asset.name }) });
-  }
-
-  function handleTenant(event) {
-    let tenant = event.target.value;
-    updateState(draftState => { draftState.dealManagement.dealForm.tenant = tenant });
-  }
-
   function searchForAssets(searchText) {
     updateState(draftState => { draftState.isAssetSearchLoading = true });
     setTimeout(() => updateState(draftState => { draftState.isAssetSearchLoading= false }), 1000);
@@ -134,6 +100,74 @@ function App() {
   function assetSearchValidateStatus() {
     return state.isAssetSearchLoading ? "validating" : "";
   }
+
+  function selectedAsset() {
+    let assets = state.dealManagement.dealForm.assets;
+
+    return assets[0] ? assets[0].name : undefined;
+  }
+
+  return <Modal 
+    visible={state.isDealModalVisible} 
+    onCancel={hideForm} 
+    okText={"Save"} 
+    onOk={saveDeal}>
+
+      <Form layout="vertical">
+
+        <Form.Item label="Add deal" />
+
+        <Form.Item>
+          <Input placeholder="Size" value={state.dealManagement.dealForm.size} onChange={handleSize}/>
+        </Form.Item>
+
+        <Form.Item>
+          <Input placeholder="Tenant" value={state.dealManagement.dealForm.tenant} onChange={handleTenant}/>              
+        </Form.Item>
+
+        <Form.Item hasFeedback validateStatus={assetSearchValidateStatus()}>
+          <Search placeholder="Search for assets" onChange={searchForAssets} />
+        </Form.Item>
+      
+        <Form.Item required={true}>
+          <Select placeholder="Select Asset" style={{ width: 200 }} value={selectedAsset()} onChange={handleAsset}>
+            <Option value={assets[0].name}>{assets[0].name}</Option>
+            <Option value={assets[1].name}>{assets[1].name}</Option>        
+          </Select>
+        </Form.Item>
+
+        { state.dealManagement.errors.length > 0 ? <Alert message={state.dealManagement.errors[0]} type="error" /> : null }
+    </Form>
+
+  </Modal>;
+}
+
+function App() {
+  let [state, setState] = useState({ 
+    dealManagement, 
+    isDealModalVisible: false, 
+    isAssetSearchLoading: false
+  });
+
+  function updateState(updateFunc) {
+    setState(produce(state, updateFunc));
+  }
+
+  let appState = [
+    state,
+    updateState
+  ];
+
+  useEffect(() => {
+    updateState(draftState => draftState.dealManagement.viewDeals());
+  }, []);  
+
+  function showNewDealForm() {
+    updateState(draftState => { 
+      draftState.dealManagement.makeNewDeal() ;
+      draftState.isDealModalVisible = true;
+    });
+  }  
 
   return (
     <StateContext.Provider value={appState}>
@@ -149,40 +183,7 @@ function App() {
           </Menu>
         </Header>
         <Content style={{ padding: '0 50px', marginTop: 64 }}>        
-          <Modal 
-            visible={state.isDealModalVisible} 
-            onCancel={hideForm} 
-            okText={"Save"} 
-            onOk={saveDeal}>
-
-              <Form layout="vertical">
-
-                <Form.Item label="Add deal" />
-
-                <Form.Item>
-                  <Input placeholder="Size" value={state.dealManagement.dealForm.size} onChange={handleSize}/>
-                </Form.Item>
-
-                <Form.Item>
-                  <Input placeholder="Tenant" value={state.dealManagement.dealForm.tenant} onChange={handleTenant}/>              
-                </Form.Item>
-
-                <Form.Item hasFeedback validateStatus={assetSearchValidateStatus()}>
-                  <Search placeholder="Search for assets" onChange={searchForAssets} />
-                </Form.Item>
-              
-                <Form.Item required={true}>
-                  <Select placeholder="Select Asset" style={{ width: 200 }} value={state.dealManagement.dealForm.assets[0]} onChange={handleAsset}>
-                    <Option value={assets[0].name}>{assets[0].name}</Option>
-                    <Option value={assets[1].name}>{assets[1].name}</Option>        
-                  </Select>
-                </Form.Item>
-
-                { state.dealManagement.errors.length > 0 ? <Alert message={state.dealManagement.errors[0]} type="error" /> : null }
-            </Form>
-
-          </Modal>
-          <StateButton />
+          <DealForm />
           <div style={ { margin: 24, background: '#fff' } }>          
             <DealList deals={state.dealManagement.deals} showNewDealForm={showNewDealForm} />
           </div>
